0173743791ef5dc51d606a68835ff63d0b48466d
andriikamaldinov <akam@jsdaddy.io>
2023-04-10 11:15:31 +0300

    feat(no-ref): change chip component

---
+++ open-source/chip/chip.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'jsdaddy-open-source-chip[chip]',
+    standalone: true,
+    templateUrl: './chip.component.html',
+    styleUrls: ['./chip.component.scss'],
+})
+export class ChipComponent {
+    @Input()
+    public chip!: string;
+    @Input()
+    public chipBgColor = 'rgba(0,0,0,0.05)';
+    @Input()
+    public chipTextColor = '#191919';
+    @Input()
+    public isActive = false;
+    @Input()
+    public pointer = false;
+}
--- open-source/sub-header/sub-header.component.ts
+++ open-source/sub-header/sub-header.component.ts
@@ -1,19 +1,20 @@
-import { Component, Input } from '@angular/core';
-import { NgOptimizedImage } from '@angular/common';
-import { ChipsComponent } from '@open-source/chips/chips.component';
+import { Component, inject, Input } from '@angular/core';
+import { NgFor, NgOptimizedImage } from '@angular/common';
 import { AssetPipe } from '@libraries//asset/asset.pipe';
+import { TrackByService } from '@libraries/track-by/track-by.service';
+import { ChipComponent } from '@open-source/chip/chip.component';
 
 @Component({
     selector: 'jsdaddy-open-source-sub-header',
     templateUrl: './sub-header.component.html',
     styleUrls: ['./sub-header.component.scss'],
     standalone: true,
-    imports: [NgOptimizedImage, ChipsComponent, AssetPipe],
+    imports: [NgOptimizedImage, NgFor, ChipComponent, AssetPipe],
 })
 export class SubHeaderComponent {
     @Input() public title!: string;
-
     @Input() public subtitle!: string;
-
     @Input() public chips!: string[];
+
+    public readonly trackByPath = inject(TrackByService).trackBy('chip');
 }
