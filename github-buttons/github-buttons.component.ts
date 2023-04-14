8546820c889e6cf7f40795305428b138b436d274
andriikamaldinov1 <akam@jsdaddy.io>
2023-04-14 19:01:57 +0300

    fix(no-ref): fix folders and add input value for button

--- github-buttons/github-buttons.component.ts
+++ github-buttons/github-buttons.component.ts
@@ -1,18 +1,19 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { AsyncPipe, NgOptimizedImage } from '@angular/common';
 import { Observable } from 'rxjs';
-import { GithubStarsService } from '@libraries/github-stars/github-stars.service';
 import { AssetPipe } from '@libraries/asset/asset.pipe';
 import { SharedAssetPath } from '@libraries/asset/asset.path';
+import { GithubStarsService } from '@libraries/github/github-stars.service';
 
 @Component({
-    selector: 'jsdaddy-github-stars',
-    templateUrl: './github-stars.component.html',
-    styleUrls: ['./github-stars.component.scss'],
+    selector: 'jsdaddy-github-buttons[title]',
+    templateUrl: './github-buttons.component.html',
+    styleUrls: ['./github-buttons.component.scss'],
     standalone: true,
     imports: [AsyncPipe, NgOptimizedImage, AssetPipe],
 })
-export class GithubStarsComponent {
+export class GithubButtonsComponent {
+    @Input() public title!: string;
     public readonly assetPathShared = SharedAssetPath.ROOT;
     public readonly jsdaddyGithub = 'https://github.com/JsDaddy/';
     public readonly countOfStarsOnGithub$: Observable<number> =
--- open-source/sub-header/sub-header.component.ts
+++ open-source/sub-header/sub-header.component.ts
@@ -3,14 +3,14 @@ import { AsyncPipe, NgFor, NgOptimizedImage } from '@angular/common';
 import { AssetPipe } from '@libraries/asset/asset.pipe';
 import { TrackByService } from '@libraries/track-by/track-by.service';
 import { ChipComponent } from '@libraries/chip/chip.component';
-import { GithubStarsComponent } from '@libraries/github-stars/github-stars.component';
+import { GithubButtonsComponent } from '@libraries/github-buttons/github-buttons.component';
 
 @Component({
     selector: 'jsdaddy-open-source-sub-header',
     templateUrl: './sub-header.component.html',
     styleUrls: ['./sub-header.component.scss'],
     standalone: true,
-    imports: [NgOptimizedImage, NgFor, AsyncPipe, ChipComponent, AssetPipe, GithubStarsComponent],
+    imports: [NgOptimizedImage, NgFor, AsyncPipe, ChipComponent, AssetPipe, GithubButtonsComponent],
 })
 export class SubHeaderComponent {
     @Input() public title!: string;
