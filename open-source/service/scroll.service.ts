76de5fd036ff3e068624988ee4485ffc343157d7
andriikamaldinov1 <akam@jsdaddy.io>
2023-04-18 13:40:22 +0300

    fix(no-ref): add new styles for scroll

--- open-source/service/scroll.service.ts
+++ open-source/service/scroll.service.ts
@@ -2,8 +2,9 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ScrollService {
+    private readonly minusTopHeight = 300;
     public isInViewport(elm: HTMLElement) {
-        const elementTop = elm.offsetTop - 300;
+        const elementTop = elm.offsetTop - this.minusTopHeight;
         const elementBottom = elementTop + elm.offsetHeight;
         const viewportTop = document.documentElement.scrollTop;
         const viewportBottom = viewportTop + document.documentElement.clientHeight;
