e116683deda89e6d6d9baff3c123205ddcb59941
andriikamaldinov1 <akam@jsdaddy.io>
2023-04-21 13:42:06 +0300

    fix(no-ref): fix after qa

--- body-styles/body-styles.service.ts
+++ body-styles/body-styles.service.ts
@@ -4,9 +4,11 @@ import { Injectable } from '@angular/core';
 export class BodyStylesService {
     public setOverflowYBody(hidden: boolean): void {
         const body = document.querySelector('body');
-        if (!body) {
+        const html = document.querySelector('html');
+        if (!body || !html) {
             return;
         }
         body.style.overflowY = hidden ? 'hidden' : 'overlay';
+        html.style.overflowY = hidden ? 'hidden' : 'overlay';
     }
 }
