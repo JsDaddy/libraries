de25f29cb4560f4acccd7960701ce66752f9647c
andriikamaldinov1 <akam@jsdaddy.io>
2023-04-24 17:23:47 +0300

    fix(no-ref): delete scrollService

--- open-source/on-scroll/on-scroll.service.ts
+++ open-source/on-scroll/on-scroll.service.ts
@@ -1,28 +1,35 @@
 import { ElementRef, inject, Injectable, QueryList } from '@angular/core';
-import { debounceTime, fromEvent, takeUntil } from 'rxjs';
+import { BehaviorSubject, debounceTime, fromEvent, takeUntil } from 'rxjs';
 import { UnSubscriber } from '@libraries/unsubscriber/unsubscriber.service';
-import { ScrollService } from '@open-source/service/scroll.service';
 import { Router } from '@angular/router';
 
 @Injectable()
 export class OnScrollService extends UnSubscriber {
-    public activeCardId = 1;
-    private readonly scrollService = inject(ScrollService);
+    public activeCardId: BehaviorSubject<number> = new BehaviorSubject(1);
+
     private readonly router = inject(Router);
+    private readonly minusTopHeight = 300;
 
     public onScroll(cards: QueryList<ElementRef>): void {
         fromEvent(document, 'scroll')
             .pipe(debounceTime(100), takeUntil(this.unsubscribe$$))
             .subscribe(() => {
-                const scrollIdCard = cards.find((e) =>
-                    this.scrollService.isInViewport(e.nativeElement)
-                )?.nativeElement.id;
-                if (this.activeCardId !== Number(scrollIdCard)) {
-                    this.activeCardId = Number(scrollIdCard);
+                const scrollIdCard = cards.find((e) => this.isInViewport(e.nativeElement))
+                    ?.nativeElement.id;
+                if (this.activeCardId.value !== Number(scrollIdCard)) {
+                    this.activeCardId.next(Number(scrollIdCard));
                     this.router.navigate(['/'], {
                         fragment: scrollIdCard,
                     });
                 }
             });
+    }
+
+    public isInViewport(elm: HTMLElement) {
+        const elementTop = elm.offsetTop - this.minusTopHeight;
+        const elementBottom = elementTop + elm.offsetHeight;
+        const viewportTop = document.documentElement.scrollTop;
+        const viewportBottom = viewportTop + document.documentElement.clientHeight;
+        return elementBottom > viewportTop && elementTop < viewportBottom;
     }
 }
