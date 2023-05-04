3b869b8d416ded3b11a01019da1df551babf4eaf
andriikamaldinov1 <akam@jsdaddy.io>
2023-05-04 12:20:26 +0300

    fix(no-ref): fix qa

--- open-source/scroll/scroll.service.ts
+++ open-source/scroll/scroll.service.ts
@@ -8,6 +8,7 @@ export class ScrollService extends UnSubscriber {
     private readonly activeCardId$$: BehaviorSubject<number> = new BehaviorSubject(1);
     private readonly router = inject(Router);
     private readonly minusTopHeight = 300;
+    private readonly minusTopMobileHeight = 150;
 
     public readonly activeCard$ = this.activeCardId$$.asObservable();
 
@@ -17,7 +18,10 @@ export class ScrollService extends UnSubscriber {
             .subscribe(() => {
                 const scrollIdCard = cards.find((e) => this.isInViewport(e.nativeElement))
                     ?.nativeElement.id;
-                if (this.activeCardId$$.value !== Number(scrollIdCard)) {
+                if (
+                    this.activeCardId$$.value !== Number(scrollIdCard) &&
+                    scrollIdCard !== undefined
+                ) {
                     this.activeCardId$$.next(Number(scrollIdCard));
                     this.router.navigate(['/'], {
                         fragment: scrollIdCard,
@@ -29,12 +33,13 @@ export class ScrollService extends UnSubscriber {
     public isInViewport(elm: HTMLElement) {
         const windowHeight = window.document.body.offsetHeight;
         let elementTop = elm.offsetTop - this.minusTopHeight;
-        if (windowHeight < 450) {
-            elementTop = elm.offsetTop - 150;
-        }
         const elementBottom = elementTop + elm.offsetHeight;
+
         const viewportTop = document.documentElement.scrollTop;
         const viewportBottom = viewportTop + document.documentElement.clientHeight;
+        if (windowHeight < 450) {
+            elementTop = elm.offsetTop - this.minusTopMobileHeight;
+        }
         return elementBottom > viewportTop && elementTop < viewportBottom;
     }
 }
