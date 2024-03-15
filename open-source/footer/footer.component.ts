513eb6f1f19917affa8a80abaf41c6958b591385
andriikamaldinov1 <akam@jsdaddy.io>
2024-03-15 18:47:23 +0200

    feat(ref: no-ref): remove transferKey

--- open-source/footer/footer.component.ts
+++ open-source/footer/footer.component.ts
@@ -1,5 +1,4 @@
-import { Component, inject, makeStateKey, OnInit, PLATFORM_ID, TransferState } from '@angular/core';
-import { isPlatformServer } from '@angular/common';
+import { Component, inject } from '@angular/core';
 import { VersionToken } from '@libraries/version/version.token';
 
 @Component({
@@ -8,18 +7,10 @@ import { VersionToken } from '@libraries/version/version.token';
     styleUrl: './footer.component.scss',
     standalone: true,
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent {
     public readonly copyrightText = `© JSdaddy, 2016-${new Date().getFullYear()}, All Rights Reserved`;
-    private readonly transferState = inject(TransferState);
-    private readonly versionKey = makeStateKey<string>('version');
-    private readonly platformId = inject<string>(PLATFORM_ID);
+
     private readonly versionValue = inject(VersionToken, { optional: true });
 
-    public version = this.transferState.get<string>(this.versionKey, '*.*.*');
-
-    public ngOnInit(): void {
-        if (isPlatformServer(this.platformId)) {
-            this.transferState.set(this.versionKey, `v${this.versionValue || '*.*.*'}`);
-        }
-    }
+    public readonly version = `v${this.versionValue || '*.*.*'}`;
 }
