0598d0a00ab5a3a6b6246399c599b8985426d9e3
andriikamaldinov1 <akam@jsdaddy.io>
2024-03-15 17:32:18 +0200

    feat(ref: no-ref): add versions to footer

--- open-source/footer/footer.component.ts
+++ open-source/footer/footer.component.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
-import { AssetPipe } from '../../asset/asset.pipe';
-import { OpenSourcePath } from '../path/open-source.path';
-import { NgOptimizedImage } from '@angular/common';
+import { Component, inject, makeStateKey, OnInit, PLATFORM_ID, TransferState } from '@angular/core';
+import { isPlatformServer } from '@angular/common';
+import { VersionToken } from '@libraries/version/version.token';
 
 @Component({
     selector: 'jsdaddy-open-source-footer',
     templateUrl: './footer.component.html',
     styleUrl: './footer.component.scss',
     standalone: true,
-    imports: [AssetPipe, NgOptimizedImage],
 })
-export class FooterComponent {
-    public readonly openSourceFooterPath = OpenSourcePath.FOOTER;
+export class FooterComponent implements OnInit {
+    public readonly copyrightText = `© JSdaddy, 2016-${new Date().getFullYear()}, All Rights Reserved`;
+    private readonly transferState = inject(TransferState);
+    private readonly versionKey = makeStateKey<string>('version');
+    private readonly platformId = inject<string>(PLATFORM_ID);
+    private readonly versionValue = inject(VersionToken, { optional: true });
+
+    public version = this.transferState.get<string>(this.versionKey, '*.*.*');
+
+    public ngOnInit(): void {
+        if (isPlatformServer(this.platformId)) {
+            this.transferState.set(this.versionKey, `v${this.versionValue || '*.*.*'}`);
+        }
+    }
 }
