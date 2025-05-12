fa42cbf3306a65f8c92da46367c9f372dedf9b90
andriikamaldinov1 <akam@jsdaddy.io>
2025-05-12 17:19:41 +0300

    fix(ref: no-ref): fix enum

--- link/link.path.ts
+++ link/link.path.ts
@@ -1,6 +1,6 @@
-export const enum LinkPath {
-    NGX_MASK = 'https://jsdaddy.github.io/ngx-mask/',
-    NGX_COPYPASTE = 'https://jsdaddy.github.io/ngx-copypaste/',
-    NGX_LOADER = 'https://jsdaddy.github.io/ngx-loader-indicator/',
-    WEBSITE_JSDDADY = 'https://jsdaddy.io/',
-}
+export const LinkPath = {
+    NGX_MASK: 'https://jsdaddy.github.io/ngx-mask/',
+    NGX_COPYPASTE: 'https://jsdaddy.github.io/ngx-copypaste/',
+    NGX_LOADER: 'https://jsdaddy.github.io/ngx-loader-indicator/',
+    WEBSITE_JSDDADY: 'https://jsdaddy.io/',
+} as const;
